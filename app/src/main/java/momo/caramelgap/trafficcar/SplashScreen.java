a3414436a478224357e88c9c3caa25fb086e87f9
caramelgap <caramelgap@gmail.com>
2016-08-21 15:04:21 +0700

    Project Traffic Car Finish

--- app/src/main/java/momo/caramelgap/trafficcar/SplashScreen.java
+++ app/src/main/java/momo/caramelgap/trafficcar/SplashScreen.java
@@ -1,5 +1,8 @@
 package momo.caramelgap.trafficcar;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+        //อยู่บน
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(SplashScreen.this, MainTraffic.class));
+                    finish();
+            }
+        },8000);  //จำนวนวินาทีที่เล่น
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        mediaPlayer.start();
+
+
+    }   //Main Method
+}   //Main Class
