6e19373df52cc6b95de503ddaf6ec7182bc41f4d
caramelgap <caramelgap@gmail.com>
2016-08-21 09:21:21 +0700

    Intent website

--- app/src/main/java/momo/caramelgap/trafficcar/MainTraffic.java
+++ app/src/main/java/momo/caramelgap/trafficcar/MainTraffic.java
@@ -1,6 +1,8 @@
 package momo.caramelgap.trafficcar;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +16,8 @@ public class MainTraffic extends AppCompatActivity {
       // Explicit การประกาศตัวแปร
     private ListView trafficListView;
     private Button abountMeButton;
+    private static final String urlYouTube = "https://youtu.be/Tf-hD_DFeyU";
+    //static  ค่าคงที่  final เปลี่ยนแปลงไม่ได้  String =Object Java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +33,19 @@ public class MainTraffic extends AppCompatActivity {
 
         //Get Event from Click Button'
         abountMeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                @Override
+                public void onClick(View view) {
 
                 //ใส่เสียง mp3 wav ชื่อพิมเล็กเท่านั้น Sound Effect
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
                 mediaPlayer.start();
 
-                //Intent to WebView https://youtu.be/Dvgb8nxi9To
+                //Intent to WebView https://youtu.be/Tf-hD_DFeyU
+                    //Intent = ประกาศ class I ใหญ่ intent = new Intent(Intent.ACV
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    // Uri คือเรียกใช้
+                    intent.setData(Uri.parse(urlYouTube));
+                    startActivity(intent);
 
             }   // onclick
         });
